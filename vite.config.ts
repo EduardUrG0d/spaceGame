bfd573af04d41c1dba49e126e51b8347adf6f8cd
EduardUrG0d <116226933+EduardUrG0d@users.noreply.github.com>
2025-06-13 11:36:43 +0300

    fix: правильный base для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/space-game/', // Имя репозитория на GitHub
+  base: '/spaceGame/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
